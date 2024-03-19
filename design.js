2be8f89ab86c4c45e83b75a7c2cebfbcdd069ab7
shakeel9051 <141891529+shakeel9051@users.noreply.github.com>
2024-03-19 09:19:26 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,8 @@
+function on(){
+    var img = document.getElementById("bulb");
+    img.src="bulbon.jpeg"
+}
+function off(){
+   var img = document.getElementById("bulb");
+   img.src="bulboff.jpeg"
+}
